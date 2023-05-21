10fb1e22a8ead1ea37ef2d1b029be1684ab47edb
sneakycrow <zach@sneakycrow.dev>
2023-05-21 13:13:51 -0700

    enable basic send commands

--- borgbot/src/stream.rs
+++ borgbot/src/stream.rs
@@ -7,10 +7,11 @@ pub(crate) async fn connect_to_twitch_chat(streamer: String) {
     // initializes the bot configuration
     let bot_config = ClientConfig::default();
     let (mut incoming_messages, client) =
-        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(bot_config);
-
+        TwitchIRCClient::<SecureTCPTransport, dyn LoginCredentials<Error=()>>::new(bot_config);
+    let mut sender = client.clone();
     // Create the handler for receiving messages. This is where you can configure what to do with a message
     let join_handle = tokio::spawn(async move {
+        sender = sender.to_owned();
         while let Some(message) = incoming_messages.recv().await {
             match message {
                 Privmsg(msg) => {
@@ -23,6 +24,13 @@ pub(crate) async fn connect_to_twitch_chat(streamer: String) {
                             server_timestamp = msg.server_timestamp.to_string(),
                             id = msg.message_id,
                         );
+                    match msg.message_text {
+                        _text if msg.message_text.to_lowercase().contains("!register") => {
+                            event!(Level::INFO, "hello command triggered");
+                            sender.say(msg.channel_login, "Hello!".parse().unwrap()).await.unwrap();
+                        }
+                        _ => {}
+                    }
                 }
                 _ => {}
             }
