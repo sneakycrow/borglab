0fc1bc01dd526cd045771391eab8346b503f979d
sneakycrow <zach@sneakycrow.dev>
2023-05-22 22:10:36 -0700

    init viewerlist component

--- borgui/src/App.tsx
+++ borgui/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ViewerList from "./components/ViewerList";
 
 function App() {
     return (
         <div className="flex flex-col items-center p-6">
             <h1 className="text-4xl font-bold">Borgui</h1>
-            <p>Eventually some stuff will be here</p>
+            <ViewerList/>
         </div>
     );
 }
