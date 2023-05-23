1a2cae9a91851e62e1413f1755a4c5556f110b5e
sneakycrow <zach@sneakycrow.dev>
2023-05-22 20:22:50 -0700

    replace scaffolding

--- borgui/src/App.tsx
+++ borgui/src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center p-6">
+            <h1 className="text-4xl font-bold">Borgui</h1>
+            <p>Eventually some stuff will be here</p>
+        </div>
+    );
 }
 
 export default App;
