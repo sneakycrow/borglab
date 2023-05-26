5c98677a3de84abb7402f982d1c12b1c9e9598a9
sneakycrow <zach@sneakycrow.dev>
2023-05-25 17:22:24 -0700

    init avatar model

---
+++ entity/src/avatar.rs
@@ -0,0 +1,35 @@
+use chrono::Utc;
+use sea_orm::entity::prelude::*;
+use sea_orm::ActiveValue::Set;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "viewers")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: Uuid,
+    pub username: String,
+    pub created_at: DateTimeUtc,
+    pub updated_at: DateTimeUtc,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::viewer::Entity",
+        from = "Column::Id",
+        to = "super::viewer::Column::Id"
+    )]
+    Viewer,
+}
+
+impl ActiveModelBehavior for ActiveModel {
+    /// Create a new ActiveModel with default values. Also used by `Default::default()`.
+    fn new() -> Self {
+        Self {
+            id: Set(Uuid::new_v4()),
+            created_at: Set(Utc::now()),
+            updated_at: Set(Utc::now()),
+            ..ActiveModelTrait::default()
+        }
+    }
+}
