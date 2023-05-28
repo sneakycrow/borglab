e0aa11ddeb69a6950afb9b40c92f4f34caf41411
sneakycrow <zach@sneakycrow.dev>
2023-05-27 21:52:54 -0700

    simplify login form

--- borgui/components/Login.tsx
+++ borgui/components/Login.tsx
@@ -1,12 +1,10 @@
-// A login form with a purple Login with twitch button
-"use client";
 import React from "react";
 import { signIn } from "next-auth/react";
 
 const Login = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Borgui</h1>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Login</h1>
       <button
         className="bg-purple-600 text-white px-4 py-2 rounded-md mt-4"
         onClick={() => signIn("twitch")}
