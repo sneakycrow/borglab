5da9b41e46f188ad6fa66d654628ba9b5fcaf78b
sneakycrow <zach@sneakycrow.dev>
2023-05-27 22:51:26 -0700

    add better placeholders

--- borgui/pages/index.tsx
+++ borgui/pages/index.tsx
@@ -1,10 +1,9 @@
 import { useSession } from "next-auth/react";
-import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 import { Suspense } from "react";
 import LoadingSpinner from "@/components/LoadingSpinner";
-import Link from "next/link";
 import Layout from "@/components/Layout";
 import Login from "@/components/Login";
+import AvatarGenerator from "@/components/AvatarGenerator";
 
 const Home = () => {
   const { data: session, status } = useSession();
@@ -16,27 +15,17 @@ const Home = () => {
     );
   return (
     <Suspense fallback={<LoadingSpinner />}>
-      <Layout title="borgui">
+      <Layout>
         <div className="flex flex-col items-center justify-center min-h-[50vh] py-2 w-full">
           {!session ? (
             <Login />
           ) : (
-            <div className="flex w-full space-x-10">
-              <Link
-                href="/"
-                className="min-h-[50vh] h-full w-full flex items-center justify-end opacity-20 hover:opacity-100 cursor-pointer transition-opacity duration-300"
-              >
-                <IconChevronLeft size={100} />
-                <h4 className="text-4xl font-extrabold">Viewer Portal</h4>
-              </Link>
-              <Link
-                href="/admin"
-                className="min-h-[50vh] h-full w-full flex items-center justify-start opacity-20 hover:opacity-100 cursor-pointer transition-opacity duration-300"
-              >
-                <h4 className="text-4xl font-extrabold">Staff Portal</h4>
-                <IconChevronRight size={100} />
-              </Link>
-            </div>
+            <>
+              <p className="text-gray-300 font-bold text-3xl">
+                Content coming soon
+              </p>
+              <AvatarGenerator />
+            </>
           )}
         </div>
       </Layout>
