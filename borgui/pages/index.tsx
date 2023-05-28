76f10e05d8249b57bfcfbb0e5d08fe20cefc4cb6
sneakycrow <zach@sneakycrow.dev>
2023-05-27 21:02:28 -0700

    change a tag to link

--- borgui/pages/index.tsx
+++ borgui/pages/index.tsx
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 import { Suspense } from "react";
 import LoadingSpinner from "@/components/LoadingSpinner";
+import Link from "next/link";
 
 const Home = () => {
   const { data: session, status } = useSession();
@@ -21,9 +22,9 @@ const Home = () => {
           <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-4xl font-bold">Borgui</h1>
             <p className="text-2xl font-bold">Welcome {session.user?.name}</p>
-            <a href="/api/auth/signout" className="text-xl text-red-500">
+            <Link href="/api/auth/signout" className="text-xl text-red-500">
               Sign out
-            </a>
+            </Link>
             <div className="flex">
               <div className="h-[50vh] w-[50vh] flex items-center justify-center opacity-20 hover:opacity-100 cursor-pointer">
                 <IconChevronLeft size={100} />
