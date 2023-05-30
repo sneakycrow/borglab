007e01ddf07adf375c7131ab2513a684388da1ac
sneakycrow <zach@sneakycrow.dev>
2023-05-29 17:42:03 -0700

    add title to pages

--- borgui/components/Layout.tsx
+++ borgui/components/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import cx from "classnames";
 import Navigation from "@/components/Navigation";
 import Credits from "@/components/Credits";
+import Head from "next/head";
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,17 +10,25 @@ interface LayoutProps {
   title?: string;
 }
 
+const DEFAULT_TITLE = "borgui";
 const Layout = (props: LayoutProps) => {
   const classes = cx(
     "flex flex-col items-center min-h-screen py-2 w-2/3 mx-auto space-y-4",
     props.className
   );
   return (
-    <main className={classes}>
-      <Navigation title={props.title ?? ""} />
-      {props.children}
-      <Credits />
-    </main>
+    <>
+      <Head>
+        <title>{`${DEFAULT_TITLE} ${
+          props.title ? `- ${props.title}` : ""
+        }`}</title>
+      </Head>
+      <main className={classes}>
+        <Navigation title={props.title ?? ""} />
+        {props.children}
+        <Credits />
+      </main>
+    </>
   );
 };
 
